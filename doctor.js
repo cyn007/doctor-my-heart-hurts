b5f44f1cf710fa6bc96a101438d9725cf2c4a13c
cyn007 <39014286+cyn007@users.noreply.github.com>
2018-06-05 20:22:16 +0200

    Add files via upload

---
+++ doctor.js
@@ -0,0 +1,43 @@
+$(document).ready(function($) {    
+
+$('.epaule').click(function() {
+$('.titretexte').html("L'epaule");
+  $('.corpstexte').html("En anatomie, le bras (parfois arrière-bras) est la partie du membre supérieur humain comprise entre l'épaule (qui l'attache au tronc) et le coude (qui l'attache à l'avant-bras). Dans le langage courant, le terme \n\« bras » peut référer au membre supérieur dans sa totalité.");
+
+});
+
+$('.coude').click(function(){
+    $('.titretexte').html("Coude");
+        $('.corpstexte').html("Le coude (ou articulation olécranienne) est la partie du membre supérieur située entre le bras et l'avant-bras. Cette articulation comprend en avant la région du « pli du coude » (ou fosse cubitale). C'est un complexe articulaire synovial du membre supérieur humain reliant le bras à l'avant-bras. Il unit ainsi trois os entre eux : le radius, l'ulna (cubitus) et l'humérus.");
+    
+});
+
+$('.main').click(function(){
+    $('.titretexte').html("Main");
+       $('.corpstexte').html(" La main est l’organe préhensile effecteur situé à l’extrémité de l’avant-bras et relié à ce dernier par le poignet. C'est un organe capable notamment de saisir et manipuler des objets. Chez l'homme, la main est un organe extrêmement développé et important, elle dispose d'une palette d'actions très large. Située à l'extrémité des deux membres supérieurs, chaque main possède cinq doigts qui apportent une contribution majeure au sens du toucher. Elle est également un moyen d’expression quand elle complète la parole, ou sert d'articulateur en langue des signes.");
+
+});
+
+$('.jambe').click(function(){
+    $('.titretexte').html("Jambe");
+       $('.corpstexte').html("C'est la partie du membre inférieur située entre la hanche et le genou. Elle est articulée sur le bassin à la partie supérieure et le genou à la partie inférieure, a une forme de cylindre et comporte un os central, le fémur, et une dizaine de muscles. La cuisse est reliée au tronc via la hanche en haut et à la jambe via le genou en bas. On lui décrit un squelette, une musculature, une vasculaisation et une innervation.");
+    
+});
+
+$('.mollet').click(function(){
+    $('.titretexte').html("Mollet");
+       $('.corpstexte').html("Le mollet désigne la partie charnue et musclée située entre le jarret (ou creux poplité) et le tendon d'Achille. C'est lui qui donne son relief à la face postérieure de la jambe.Durant la marche le mollet joue un rôle circulatoire important, la contraction des muscles jouant un rôle de pompe sur le flux sanguin.");
+});
+
+$('.cheville').click(function(){
+    $('.titretexte').html("Cheville");
+       $('.corpstexte').html("La cheville ou cou-de-pied est l'articulation qui relie la jambe et le pied. Elle est parfois sujette à des entorses, le plus souvent externes par flexion plantaire et pied en équin. Une cheville adulte est composée, au point de vue osseux, de l'épiphyse inférieure du tibia (malléole interne et plafond), de l'épiphyse inférieure de la fibula (ou péroné) (malléole externe) et du talus (ou astragale). Classiquement, on parle d'articulation supinale de l'arrière pied. On peut la définir comme une articulation synoviale de type ginglyme (anciennement trochléenne) c'est-à-dire mettant en rapport un segment de poulie plein à un segment de poulie creux.");
+});
+
+
+
+
+
+
+
+});
